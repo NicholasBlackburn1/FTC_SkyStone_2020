b64a041b23460731414f13295794aa363e427bb9
Nicholas Blackburn <nickblackburn02@gmail.com>
2019-09-20 09:30:58 -0400

    added Robot_Drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot_Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot_Drive.java
@@ -3,21 +3,27 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 import org.firstinspires.ftc.teamcode.Drivetrain.DriveTrain;
+import org.firstinspires.ftc.teamcode.Drivetrain.ImuMapper;
 import org.firstinspires.ftc.teamcode.Drivetrain.Robot_OI;
 
-public class Rovot_Function_Drive extends OpMode {
+public class Robot_Drive extends OpMode {
 
-    private static DriveTrain driveTrain = new DriveTrain();
     private static Robot_OI oi = new Robot_OI();
+    private static DriveTrain driveTrain = new DriveTrain();
+    private static ImuMapper Imu = new ImuMapper();
 
     @Override
     public void init() {
+
         oi.init();
+        Imu.init();
         driveTrain.init();
     }
 
     @Override
     public void loop() {
+        driveTrain.Motor_control();
+        Imu.ImuData();
 
     }
 }
