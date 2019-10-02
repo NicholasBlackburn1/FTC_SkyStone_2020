3ca4cb4860685f891c5051facf9677f24f18603b
Nicholas Blackburn <nickblackburn02@gmail.com>
2019-10-02 10:01:49 -0400

    Update Pid_Controller.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drivetrain/Pid_Controller.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drivetrain/Pid_Controller.java
@@ -1,13 +1,26 @@
 package org.firstinspires.ftc.teamcode.Drivetrain;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.PIDCoefficients;
+import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 
 public class Pid_Controller extends OpMode {
+
+    PIDFCoefficients pidCoefficients = new PIDFCoefficients();
+    
+    public void PID_input(){
+
+
+    }
+
+    public void PID_OUTPUT(){
+
+    }
 
 
     @Override
     public void init() {
-        
+
     }
 
     @Override
