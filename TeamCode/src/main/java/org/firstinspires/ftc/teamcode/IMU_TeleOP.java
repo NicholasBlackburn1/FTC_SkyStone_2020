290ee15cc3e913f648e8b6719934871d83fa0eb1
Ftc12578JohnF <55156408+Ftc12578JohnF@users.noreply.github.com>
2019-10-14 15:32:36 -0400

    er

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IMU_TeleOP.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.teamcode.Drivetrain.DriveTrain;
+
+public class IMU_TeleOP extends OpMode {
+
+    DriveTrain driveTrain = new DriveTrain();
+    
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
