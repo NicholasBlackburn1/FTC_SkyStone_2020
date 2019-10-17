82e8e61a6a02d3cd340570dd54291517e1b5d0ad
Ftc12578JohnF <55156408+Ftc12578JohnF@users.noreply.github.com>
2019-10-17 13:50:32 -0400

    Update Skeltion_Code.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Skeltion_Code.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode.TeleOp;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.teamcode.Drivetrain.DriveTrain;
+
+public class Skeltion_Code extends OpMode
+{
+    DriveTrain driveTrain = new DriveTrain();
+        
+
+    @Override
+    public void init() {
+
+
+
+    }
+
+    @Override
+    public void loop() {
+
+
+
+    }
+}
