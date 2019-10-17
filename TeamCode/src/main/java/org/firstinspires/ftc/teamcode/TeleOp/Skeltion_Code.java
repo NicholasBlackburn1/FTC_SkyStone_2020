5c92ceb613d5fbc89bdd8c8868834686b169760d
Nicholas Blackburn <nickblackburn02@gmail.com>
2019-10-17 13:49:57 -0400

    added skellcode
    skel code for main robot teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Skeltion_Code.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Skeltion_Code.java
@@ -1,11 +1,15 @@
 package org.firstinspires.ftc.teamcode.TeleOp;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import org.firstinspires.ftc.teamcode.Drivetrain.*;
 
 public class Skeltion_Code extends OpMode
 {
+    DriveTrain drivetrain = new DriveTrain();
+
     @Override
     public void init() {
+    drivetrain.Hardwareinit(hardwareMap);
 
     }
 
