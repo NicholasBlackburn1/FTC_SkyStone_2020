ca580cc914a6461157aed54cd1d0a3a9de708fdb
Nicholas Blackburn <nickblackburn02@gmail.com>
2019-10-21 15:43:24 -0400

    updates Autio

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/Auto_2020.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/Auto_2020.java
@@ -2,14 +2,25 @@ package org.firstinspires.ftc.teamcode.Auto;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
+import org.firstinspires.ftc.teamcode.Drivetrain.DriveTrain;
+
 public class Auto_2020 extends OpMode {
+
+    DriveTrain driveTrain = new DriveTrain();
+
     @Override
     public void init() {
-        
+
+        driveTrain.Hardwareinit(hardwareMap); //init hardware for robot
+
+        driveTrain.init_Auto(4000,telemetry);// sets encoder pos
+
     }
 
     @Override
     public void loop() {
+
+        driveTrain.MotorPower(1);  //sets robot
 
     }
 }
